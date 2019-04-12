369ff20d832cbacb5863bd52e54f4b5dc935bdf2
ayushtiwari <ayushtiwari@icloud.com>
2019-04-12 18:26:01 +0530

    customer tables added

--- src/com/ayushtiwari/ManagerUI/CustomerTableController.java
+++ src/com/ayushtiwari/ManagerUI/CustomerTableController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.sql.*;
+
 public class CustomerTableController {
 
     @FXML
@@ -28,16 +30,34 @@ public class CustomerTableController {
         customerName.setCellValueFactory(new PropertyValueFactory<>("name"));
         customerCityName.setCellValueFactory(new PropertyValueFactory<>("cityName"));
         customerStreetName.setCellValueFactory(new PropertyValueFactory<>("streetName"));
-
+        consignmentId.setCellValueFactory(new PropertyValueFactory<>("consignmentId"));
         tableView.setItems(populate());
     }
 
     public ObservableList<CustomerTableItem> populate() {
         ObservableList<CustomerTableItem> observableList = FXCollections.observableArrayList();
-        /*
-         *   Query database
-         *   Get List of all customers from database
-         */
+
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/ayushtiwari/Documents/TransportCompany/TransportDatabase.db");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Customers");
+            while (resultSet.next()) {
+                observableList.add(new CustomerTableItem(
+                        resultSet.getString("name"),
+                        resultSet.getString("street"),
+                        resultSet.getString("city"),
+                        Integer.toString(resultSet.getInt("consignmentId"))
+                ));
+            }
+
+        } catch (SQLException se) {
+
+        }
+
+
         return observableList;
     }
 
