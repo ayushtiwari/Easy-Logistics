db4a0cf23280a9ced3d127dae55005f2dd6fd4ad
ayushtiwari <ayushtiwari@icloud.com>
2019-04-20 12:13:54 +0530

    manager dashboard name display

--- src/com/ayushtiwari/ManagerUI/Welcome.java
+++ src/com/ayushtiwari/ManagerUI/Welcome.java
@@ -3,6 +3,8 @@ package com.ayushtiwari.ManagerUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.sql.*;
+
 public class Welcome {
 
     @FXML
@@ -10,6 +12,19 @@ public class Welcome {
 
     public void initialize() {
 
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/ayushtiwari/Documents/TransportCompany/TransportDatabase.db");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Manager");
+
+            employeeName.setText(resultSet.getString("name"));
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong" + e.getMessage());
+        }
 
     }
 
